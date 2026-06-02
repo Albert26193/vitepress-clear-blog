24606f8eeb1c0f6364bec21cc5e8beb087bd334c
Albert26193 <56572585+Albert26193@users.noreply.github.com>
2026-06-02 12:20:40 +0800

    chore(deps): #454 upgrade all workspace dependencies to latest (#455)

--- types/style-modules/index.d.ts
+++ types/style-modules/index.d.ts
@@ -1,2 +1,5 @@
 declare module '*.css'
 declare module '*.scss'
+// Side-effect imports of package style bundles (e.g. 'vitepress-theme-link/styles');
+// TS 6 requires a declaration for these bare subpath imports (TS2882).
+declare module '*/styles'
